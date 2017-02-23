c91a95f9a2bde8d8bf6ae87c2ed75da11abdcf01
gmats <gerardo.matsui@gmail.com>
2017-02-23 14:25:13 -0300

    fix module relative path

--- src/hello/hello.component.ts
+++ src/hello/hello.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'gm-hello',
     templateUrl: './hello.component.html',
     styleUrls: ['./hello.component.less']
